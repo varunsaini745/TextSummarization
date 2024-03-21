8bcfde49c3ea4321cafa0e940816082c72239efd
varunsaini745 <ansh8063@gmail.com>
2024-03-21 13:17:35 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+from src.textSummarizer.pipeline.satge_01_data_ingestion import DataIngestionTrainingPipeline
+from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.textSummarizer.logging import logger
-logger.info("Logging from main.py")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion =DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+     
+     
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation =DataValidationTrainingPipeline()
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+    
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: str
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
