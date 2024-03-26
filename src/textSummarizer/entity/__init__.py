e3194035dd9c2dcc2665e4025cfcc4174678dbf5
varunsaini745 <ansh8063@gmail.com>
2024-03-26 17:45:42 +0530

    training stage added

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -13,3 +13,25 @@ class DataValidationConfig:
     root_dir: str
     STATUS_FILE: str
     ALL_REQUIRED_FILES: list
+    
+@dataclass(frozen=True)
+class DataTransformationConfig():
+    root_path: Path
+    data_path: Path
+    tokenizer_path: Path   
+    
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    data_path:Path
+    model_ckpt: Path
+    num_train_epochs: int
+    warmup_steps: int
+    per_device_train_batch_size: int
+    weight_decay: float
+    logging_steps: int
+    evaluation_strategy: str
+    eval_steps: int
+    save_steps: float
+    gradient_accumulation_steps: int
